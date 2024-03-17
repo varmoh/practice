a39225d3d65abcb1753c8349292ddebbcf4d6ea9
Varmo <101868197+varmoh@users.noreply.github.com>
2024-03-17 11:59:15 +0200

    re-do

---
+++ Random_image_with_python/tractor_facts_lt.py
@@ -0,0 +1,23 @@
+tractor_facts_lt = [
+    "Pirmasis benzininis traktorius buvo išrastas 1892 metais Džonu Froeliu.",
+    "Didžiausias pasaulyje traktorius, Big Bud 747, sveria virš 100 000 svarų.",
+    "Traktoriai buvo pirmą kartą pristatyti žemės ūkiui XIX amžiaus pradžioje.",
+    "John Deere įmonė buvo įkurta 1837 metais ir yra viena pirmaujančių traktorių gamintojų.",
+    "Modernūs traktoriai gali turėti GPS sistemas, skirtas precizinei žemės ūkio veiklai.",
+    "Terminas „traktorius“ kilęs iš lotynų kalbos žodžio „trahere“, reiškiančio „traukti“.",
+    "Massey-Harris įmonė pagamino pirmąjį savas judesius turintį kombainą 1938 metais.",
+    "Pirmasis traktorius su keturiomis varomosiomis ratų ašimis buvo pristatytas 1912 metais Four Wheel Drive Auto Company.",
+    "Caterpillar Inc., žinoma dėl savo statybos įrangos, taip pat gamina žemės ūkio traktorius.",
+    "Fordson Model F, pristatytas 1917 metais, buvo pirmasis masiškai gaminamas traktorius pasaulyje.",
+    "Vidutinis modernaus traktoriaus galios matavimas yra apie 150 AG.",
+    "Pirmasis dyzelinio variklio traktorius buvo pristatytas Case įmonės 1930 metais.",
+    "White Farm Equipment įmonė gamino traktorius Oliver prekės ženklu.",
+    "Kubota Corporation, įkurta 1890 metais, yra pagrindinis kompaktinių traktorių gamintojas.",
+    "Allis-Chalmers įmonė pagamino modelį WC, populiarų traktorių 1940-aisiais.",
+    "Traktoriai su priekiniais krautuvais dažnai naudojami medžiagų tvarkymui ūkiuose.",
+    "Tarptautinė Harvester Company, vėliau žinoma kaip Case IH, pagamino traktorių seriją Farmall.",
+    "Traktoriai daugelyje pasaulio vietų pakeitė gyvulių trauką žemės ūkyje.",
+    "Ferguson TE20, taip pat žinomas kaip Little Grey Fergie, buvo populiarus traktorius po karo.",
+    "Žemės ūkio revoliucija XVIII amžiuje lėmė modernių žemės ūkio mašinų plėtrą.",
+    "Traktorių traukimo varžybos apima modifikuotus traktorius, tempiančius sunkų slepą."
+]
